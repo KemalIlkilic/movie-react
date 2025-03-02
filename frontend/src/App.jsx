9c56cab6f8e1d786ee6385901efe829edcf7030a
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-03-02 18:24:47 +0300

    Implement Home page and MovieCard component; update App to render Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>This is a React app.</p>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
