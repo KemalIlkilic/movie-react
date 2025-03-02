e542c03c2e348e4c96c1d4c3d5bcdb2bbc9ecf67
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-03-02 19:51:55 +0300

    Add routing and favorites page; implement search functionality in Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,46 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
 function Home() {
+  //Triggers re-renders when updated
+  /* 
+  What Gets Re-rendered
+  When state changes in a component, React will:
+  
+  Re-render that specific component
+  Re-render all of its child components (by default) 
+  */
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1999" },
     { id: 3, title: "The Matrix", release_date: "1998" },
   ];
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="search-input"
+          type="text"
+          placeholder="Search for movies"
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
       <div className="movies-grid">
         {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
